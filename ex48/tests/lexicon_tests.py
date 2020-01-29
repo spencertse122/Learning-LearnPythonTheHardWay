db1bc78ff6f67592910483aea5049782c5696422
Spencer Tse <spencertse122@gmail.com>
2020-01-28 22:20:58 -0500

    started lexicon.py on ex48 project

--- ex48/tests/lexicon_tests.py
+++ ex48/tests/lexicon_tests.py
@@ -2,13 +2,13 @@ from nose.tools import *
 from ex48 import lexicon
 
 
-# def test_directions():
-#     assert_equal(lexicon.scan("north"), [('direction', 'north')])
-#     result = lexicon.scan("north south east")
-#     assert_equal(result, [('direction', 'north'),
-#                           ('direction', 'south'),
-#                           ('direction','east')])
-#
+def test_directions():
+    assert_equal(lexicon.scan("north"), [('direction', 'north')])
+    result = lexicon.scan("north south east")
+    assert_equal(result, [('direction', 'north'),
+                          ('direction', 'south'),
+                          ('direction','east')])
+
 # def test_verbs():
 #     assert_equal(lexicon.scan("go"), [('verb', 'go')])
 #     result = lexicon.scan("go kill eat")

bcab637d51ef605ace2099d620a6d2a9ca73903f
Spencer Tse <spencertse122@gmail.com>
2020-06-23 10:23:07 -0400

    change directories

--- ex7.py
+++ ex7.py
@@ -18,6 +18,6 @@ end12 = "r"
 
 # watch end = ' ' at the end try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6)
-print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)

ef078779754842c6dba5f2cf4ee76af783197fdd
Spencer Tse <spencertse122@gmail.com>
2021-04-19 00:49:43 -0400

    finishd Flaskr tutorial, ready to deploy

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -26,16 +26,20 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'This is me calling from mars - Spencer'
+        return 'Hello, World!'
 
-    @app.route('/')
-    def landingPage():
-        return 'Main Page'
+    # @app.route('/')
+    # def landingPage():
+    #     return 'Main Page'
 
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -28,7 +28,7 @@ def register():
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password) VALUES (?. ?)',
+                'INSERT INTO user (username, password) VALUES (?, ?)',
                 (username, generate_password_hash(password))
             )
             db.commit()

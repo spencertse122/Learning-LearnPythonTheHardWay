fde9aec07f63f74e47d627dfc654984d92489a58
Spencer Tse <spencertse122@gmail.com>
2021-04-24 15:27:33 -0400

    Finished testing ex51

---
+++ ex51/projects/gothonweb/tests/app_test.py
@@ -0,0 +1,18 @@
+import pytest
+from app import app
+
+app.config['TESTING'] = True
+web = app.test_client()
+
+def test_index():
+    rv = web.get('/', follow_redirects=True)
+    assert rv.status_code == 404
+
+    rv = web.get('/hello', follow_redirects=True)
+    assert rv.status_code == 200
+    assert b"Fill Out This Form" in rv.data
+
+    data = {'name': 'Zed', 'greet': 'Hola'}
+    rv = web.post('/hello', follow_redirects=True, data=data)
+    assert b"Zed" in rv.data
+    assert b"Hola" in rv.data
